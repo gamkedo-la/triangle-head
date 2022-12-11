ecd93e552bc6c51f5648fee2d677bd2681e910de
Ashleigh Morris <ash.morris255@gmail.com>
2022-12-11 17:57:50 -0500

    implemented follow cam code from uni docs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,7 +6,9 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform target;
-    public int offset;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,14 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FollowTarget(target);        
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
+
+    /*
 
     public void FollowTarget(Transform t){
         Vector3 localPos = transform.localPosition;
@@ -26,4 +34,5 @@ public class CameraFollow : MonoBehaviour
         //transform.localPosition = Vector3.SmoothDamp(localPos, new Vector3(targetLocalPos.x + offset.x, targetLocalPos.y + offset.y, ))
         //just look up SmoothDamp in the docs, see what the example code looks like, can you adapt it?
     }
+    */
 }

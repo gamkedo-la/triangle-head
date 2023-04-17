58bca53103378a20a77741dd098ccc1b6834c060
Ashleigh Morris <ash.morris255@gmail.com>
2023-04-16 20:59:50 -0400

    restrict player movement to a slightly smaller box than the viewport

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -109,11 +109,18 @@ public class PlayerMovement : MonoBehaviour
         //^ this might do the trick is LocalMove is called every Update
     }
 
-    void ClampPosition(){
+    /*void ClampPosition(){
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         //pos.z = Mathf.Clamp01(pos.z);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }*/
+
+    void ClampPosition(){
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.2f, 0.8f);
+        pos.y = Mathf.Clamp(pos.y, 0.2f, 0.8f);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 

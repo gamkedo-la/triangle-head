ffdff667ed68892656eb4becc7233571e4c650e4
Ashleigh Morris <ash.morris255@gmail.com>
2023-04-20 22:03:04 -0400

    commented out improvement to player restriction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -118,10 +118,19 @@ public class PlayerMovement : MonoBehaviour
     }*/
 
     void ClampPosition(){
+
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp(pos.x, 0.2f, 0.8f);
         pos.y = Mathf.Clamp(pos.y, 0.2f, 0.8f);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+        /*
+        Vector3 pos = transform.position; //have to make a copy first
+        pos.x = Mathf.Clamp(pos.x, leftEdge.position.x, rightEdge.position.x);
+        pos.y = Mathf.Clamp(pos.y, topEdge.position.y, bottomEdge.position.y);
+        transform.position = pos;
+        */
+
     }
 
 

eecbb66f8c79b8852bab36448ad26f65df3d9b26
Ashleigh Morris <ash.morris255@gmail.com>
2023-05-01 14:13:44 -0400

    second draft bounding box level 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -129,17 +129,19 @@ public class PlayerMovement : MonoBehaviour
 
     void ClampPosition(){
 
+        /*
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp(pos.x, 0.2f, 0.8f);
         pos.y = Mathf.Clamp(pos.y, 0.2f, 0.8f);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+        */
         
-        /*
+        
         Vector3 pos = transform.position; //have to make a copy first
         pos.x = Mathf.Clamp(pos.x, leftBoundary.position.x, rightBoundary.position.x);
         pos.y = Mathf.Clamp(pos.y, bottomBoundary.position.y, topBoundary.position.y);
         transform.position = pos;
-        */
+        
         
         
 

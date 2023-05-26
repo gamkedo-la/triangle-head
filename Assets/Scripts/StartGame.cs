3c8cd6354a3ddfd52fb9b612a844f8ca88c537a4
Ashleigh Morris <ash.morris255@gmail.com>
2023-05-25 20:42:37 -0400

    can navigate to level 1 and level 2 from title screen

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -14,8 +14,10 @@ public class StartGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Alpha1)) {
             SceneManager.LoadScene("level 1");
+        } else if (Input.GetKey(KeyCode.Alpha2)) {
+            SceneManager.LoadScene("level 2");
         }
     }
 }
